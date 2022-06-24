0488908a51ab45dc8d29300eb90837b692a50ccd
Geoffrey Mainland <mainland@apeiron.net>
2022-06-24 09:01:14 -0400

    Transmitters now take symbol rate and excess bandwidth parameters.

--- transmitters.py
+++ transmitters.py
@@ -4,8 +4,6 @@ from scipy.signal import get_window
 from gnuradio import gr, blocks, digital, analog, filter
 from gnuradio.filter import firdes
 import mapper
-sps = 8
-ebw = 0.35
 
 class transmitter_mapper(gr.hier_block2):
     def __init__(self, modtype, symvals, txname, samples_per_symbol=2, excess_bw=0.35):
@@ -28,37 +26,37 @@ class transmitter_mapper(gr.hier_block2):
 
 class transmitter_bpsk(transmitter_mapper):
     modname = "BPSK"
-    def __init__(self):
+    def __init__(self, samples_per_symbol=8, excess_bw=0.35):
         transmitter_mapper.__init__(self, mapper.BPSK,
-            [0,1], "transmitter_bpsk", sps, ebw)
+            [0,1], "transmitter_bpsk", samples_per_symbol, excess_bw)
 
 class transmitter_qpsk(transmitter_mapper):
     modname = "QPSK"
-    def __init__(self):
+    def __init__(self, samples_per_symbol=8, excess_bw=0.35):
         transmitter_mapper.__init__(self, mapper.QPSK,
-            [0,1,3,2], "transmitter_qpsk", sps, ebw)
+            [0,1,3,2], "transmitter_qpsk", samples_per_symbol, excess_bw)
 
 class transmitter_8psk(transmitter_mapper):
     modname = "8PSK"
-    def __init__(self):
+    def __init__(self, samples_per_symbol=8, excess_bw=0.35):
         transmitter_mapper.__init__(self, mapper.PSK8,
-            [0,1,3,2,7,6,4,5], "transmitter_8psk", sps, ebw)
+            [0,1,3,2,7,6,4,5], "transmitter_8psk", samples_per_symbol, excess_bw)
 
 class transmitter_pam4(transmitter_mapper):
     modname = "PAM4"
-    def __init__(self):
+    def __init__(self, samples_per_symbol=8, excess_bw=0.35):
         transmitter_mapper.__init__(self, mapper.PAM4,
-            [0,1,3,2], "transmitter_pam4", sps, ebw)
+            [0,1,3,2], "transmitter_pam4", samples_per_symbol, excess_bw)
 
 class transmitter_qam16(transmitter_mapper):
     modname = "QAM16"
-    def __init__(self):
+    def __init__(self, samples_per_symbol=8, excess_bw=0.35):
         transmitter_mapper.__init__(self, mapper.QAM16,
-            [2,6,14,10,3,7,15,11,1,5,13,9,0,4,12,8], "transmitter_qam16", sps, ebw)
+            [2,6,14,10,3,7,15,11,1,5,13,9,0,4,12,8], "transmitter_qam16", samples_per_symbol, excess_bw)
 
 class transmitter_qam64(transmitter_mapper):
     modname = "QAM64"
-    def __init__(self):
+    def __init__(self, samples_per_symbol=8, excess_bw=0.35):
         transmitter_mapper.__init__(self, mapper.QAM64,
             [0,32,8,40,3,35,11,43,
              48,16,56,24,51,19,59,27,
@@ -67,25 +65,25 @@ class transmitter_qam64(transmitter_mapper):
             2,34,10,42,1,33,9,41,
             50,18,58,26,49,17,57,25,
             14,46,6,38,13,45,5,37,
-            62,30,54,22,61,29,53,21], "transmitter_qam64", sps, ebw)
+            62,30,54,22,61,29,53,21], "transmitter_qam64", samples_per_symbol, excess_bw)
 
 class transmitter_gfsk(gr.hier_block2):
     modname = "GFSK"
-    def __init__(self):
+    def __init__(self, samples_per_symbol=8, excess_bw=0.35):
         gr.hier_block2.__init__(self, "transmitter_gfsk",
             gr.io_signature(1, 1, gr.sizeof_char),
             gr.io_signature(1, 1, gr.sizeof_gr_complex))
         self.repack = blocks.unpacked_to_packed_bb(1, gr.GR_MSB_FIRST)
-        self.mod = digital.gfsk_mod(sps, sensitivity=0.1, bt=ebw)
+        self.mod = digital.gfsk_mod(samples_per_symbol, sensitivity=0.1, bt=excess_bw)
         self.connect( self, self.repack, self.mod, self )
 
 class transmitter_cpfsk(gr.hier_block2):
     modname = "CPFSK"
-    def __init__(self):
+    def __init__(self, samples_per_symbol=8):
         gr.hier_block2.__init__(self, "transmitter_cpfsk",
             gr.io_signature(1, 1, gr.sizeof_char),
             gr.io_signature(1, 1, gr.sizeof_gr_complex))
-        self.mod = analog.cpfsk_bc(0.5, 1.0, sps)
+        self.mod = analog.cpfsk_bc(0.5, 1.0, samples_per_symbol)
         self.connect( self, self.mod, self )
 
 class transmitter_fm(gr.hier_block2):
